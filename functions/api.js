ade9cb664325057ca52d09052b5f399ab292ba94
XVCHub <oanruc206@gmail.com>
2026-02-07 07:32:03 +0300

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -2,6 +2,7 @@ export async function onRequestPost(context) {
   const COUNT = await context.env.POST_COUNTER.get("count") || "0"
   const NEW_COUNT = parseInt(COUNT) + 1
   await context.env.POST_COUNTER.put("count", NEW_COUNT.toString())
+  
   return new Response(JSON.stringify({
     success: true,
     total_posts: NEW_COUNT
@@ -15,6 +16,7 @@ export async function onRequestPost(context) {
 
 export async function onRequestGet(context) {
   const COUNT = await context.env.POST_COUNTER.get("count") || "0"
+  
   return new Response(JSON.stringify({
     total_posts: parseInt(COUNT)
   }), {
